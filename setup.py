77fc5b71f9988c3ddc78a81a3ad88ba3f1db76dc
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-06 00:51:59 +0300

    new template

--- setup.py
+++ setup.py
@@ -2,26 +2,27 @@ import setuptools
 
 setuptools.setup(
     # the first three fields are a must according to the documentation
-    name='pyconch',
-    version='0.0.1',
+    name="pyconch",
+    version="0.0.1",
     packages=[
         'pyconch',
         'pyconch.endpoints',
     ],
     # from here all is optional
-    description='Pyconch is a wrapper for a shell in python',
-    long_description='Pyconch is a wrapper for a shell in python',
-    author='Mark Veltzer',
-    author_email='mark.veltzer@gmail.com',
-    maintainer='Mark Veltzer',
-    maintainer_email='mark.veltzer@gmail.com',
+    description="Pyconch is a wrapper for a shell in python",
+    long_description="Pyconch is a wrapper for a shell in python",
+    long_description_content_type="text/x-rst",
+    author="Mark Veltzer",
+    author_email="mark.veltzer@gmail.com",
+    maintainer="Mark Veltzer",
+    maintainer_email="mark.veltzer@gmail.com",
     keywords=[
         'make',
         'scons',
     ],
-    url='https://veltzer.github.io/pyconch',
-    download_url='https://github.com/veltzer/pyconch',
-    license='MIT',
+    url="https://veltzer.github.io/pyconch",
+    download_url="https://github.com/veltzer/pyconch",
+    license="MIT",
     platforms=[
         'python3',
     ],
@@ -40,8 +41,8 @@ setuptools.setup(
     ],
     data_files=[
     ],
-    entry_points={'console_scripts': [
+    entry_points={"console_scripts": [
         'pyconch=pyconch.endpoints.main:main',
     ]},
-    python_requires='>=3.5',
+    python_requires=">=3.5",
 )
