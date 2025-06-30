de838f6d1966d4631b11228d6e9f1b661a817bbb
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 14:09:58 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,26 +1,16 @@
 """ python deps for this project """
 
+import config.shared
+
 scripts: dict[str,str] = {
     "pyconch": "pyconch.endpoints.main:main",
 }
-config_requires: list[str] = [
-    "pyclassifiers",
-]
+
 install_requires: list[str] = [
     "pytconf",
     "prompt_toolkit",
     "ptterm",
 ]
-build_requires: list[str] = [
-    "hatch",
-    "pydmt",
-    "pymakehelper",
-    "pycmdtools",
-]
-test_requires: list[str] = [
-    "pylint",
-    "pytest",
-    "mypy",
-    "ruff",
-]
-requires = config_requires + install_requires + build_requires + test_requires
+build_requires: list[str] = config.shared.PBUILD
+test_requires: list[str] = config.shared.PTEST
+requires = install_requires + build_requires + test_requires
